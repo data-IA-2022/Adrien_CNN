4a15b42232ba14ce027b7479d502c2df5361fc34
Adrien Riguet <adrien.riguet@hotmail.com>
2023-05-22 22:19:10 +0200

    schémas

--- Accueil.py
+++ Accueil.py
@@ -1,4 +1,9 @@
+from os.path import join
+from os import listdir
+
+import pandas as pd
 import streamlit as st
+from matplotlib import pyplot as plt
 
 st.title("Bienvenue !")
 
@@ -9,3 +14,35 @@ st.write(
 L'utilisateur doit pouvoir uploader une photo et l'application doit préciser de quel animal il s'agit ainsi que la probabilité de la classification.
 Le classifieur sera développé avec Keras."""
 )
+
+st.header("Résultats des modèles")
+
+folder_name = 'models'
+
+for model_name in sorted(listdir(folder_name)):
+
+    df = pd.read_csv(join(folder_name, model_name, 'stats.csv'))
+
+    fig, ax = plt.subplots(layout='constrained')
+    ax.plot(df['epoch'], df['loss'], label='loss')
+    ax.plot(df['epoch'], df['val_loss'], label='val_loss')
+
+    ax.plot(df['epoch'], df['accuracy'], label='accuracy')
+    ax.plot(df['epoch'], df['val_accuracy'], label='val_accuracy')
+
+    df.set_index("epoch")
+
+    ax.vlines(df['val_accuracy'].idxmax(), ymin=0, ymax=1, label="max val_accuracy", color="black", linestyles='dotted')
+    ax.hlines(0.9, xmin=df['epoch'].min(), xmax=df['epoch'].max(), label="90% accuracy", color="black", linestyles='dashed')
+
+    ax.set_title(model_name)
+    ax.legend()
+
+    st.subheader(model_name)
+
+    st.pyplot(fig)
+
+    st.write(f"**Best val_accuracy :** {round(df['val_accuracy'].max(), 5)} (epoch {df['val_accuracy'].idxmax()})")
+    st.write(f"**Best val_loss :** {round(df['val_loss'].min(), 5)} (epoch {df['val_loss'].idxmin()})")
+    st.write(f"**Last val_accuracy :** {round(df['val_accuracy'][df.index.max()], 5)}")
+    st.write(f"**Last val_loss :** {round(df['val_loss'][df.index.max()], 5)}")
--- pages/1_Prédiction_d'image.py
+++ pages/1_Prédiction_d'image.py
@@ -1,4 +1,5 @@
 import os
+from os.path import join
 
 import streamlit as st
 
@@ -17,23 +18,24 @@ if img:
         from PIL import Image
         from tensorflow import keras
 
+        img_name = "img_tmp.jpg"
         PILimg = Image.open(img)
-        PILimg.save("img_tmp.jpg")
+        PILimg.save(img_name)
         PILimg.close()
 
-        model = keras.models.load_model("saves/save_at_25.keras")
+        model = keras.models.load_model(join("models", "data_augmentation_on", "Best.keras"))
 
         image_size = (180, 180)
 
         img = keras.utils.load_img(
-            "img_tmp.jpg", target_size=image_size
+            img_name, target_size=image_size
         )
         img_array = keras.utils.img_to_array(img)
         img_array = tf.expand_dims(img_array, 0)  # Create batch axis
         predictions = model.predict(img_array)
         score = float(predictions[0])
 
-        os.remove("img_tmp.jpg")
+        os.remove(img_name)
 
         cat = (1 - score)
         dog = score
